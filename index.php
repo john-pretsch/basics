e493254726ad5cf3affefd9df54425ede27d94df
John Pretsch <39734572+jpretsch@users.noreply.github.com>
2019-03-28 10:57:01 -0400

    Adding linked list and bubble sort

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ switch($t){
     case 'fib':
         include 'fibinacci.php';
         break;
+    case 'bub':
+        include 'bubble_sort.php';
+        break;
+    case 'll':
+        include 'linked_list.php';
+        break;
     default:
         echo 'Nothing to do';
 }
